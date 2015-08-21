a52f850743c1f2412cf94f55789dc7193d20c90d
prince1809 <prince18pat@gmail.com>
2015-08-21 16:49:06 +0000

    get and post request to server

--- server.js
+++ server.js
@@ -3,21 +3,62 @@
 // BASE SETUP
 
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var app = express();
+var morgan = require('morgan');
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(morgan('dev'));
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodeapp');
+
+var Bear = require('./app/models/bear');
+
 var router = express.Router();
+
+router.use(function(req,res,next){
+    
+   console.log(req.body);
+   next();
+});
 
 router.get('/',function(req,res){
     res.json({
         message: 'Welcome Prince'
     });
 });
+
+
+router.route('/bears')
+    //create a bear 
+            .post(function(req,res){
+               
+                var bear = new Bear();
+                bear.name = req.body.name;
+                bear.save(function(err){
+                    if(err)
+                        res.send(err);
+                        
+                        res.json({message: req.body});
+                })
+})
+        .get(function(req,res){
+            Bear.find(function(err,bears){
+                if(err)
+                    res.send(err);
+                res.json(bears);
+            });
+});
+
 
 app.use('/api',router);
 
