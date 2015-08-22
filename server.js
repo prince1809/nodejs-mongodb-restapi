4b1d8af927034a716e2371a5b964f23d876efe13
prince1809 <prince18pat@gmail.com>
2015-08-22 03:28:39 +0000

    PUt and delete

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ router.route('/bears')
                     if(err)
                         res.send(err);
                         
-                        res.json({message: req.body});
+                        res.json({message: 'Beer Created'});
                 })
 })
         .get(function(req,res){
@@ -56,6 +56,37 @@ router.route('/bears')
                 if(err)
                     res.send(err);
                 res.json(bears);
+            });
+});
+
+router.route('/bears/:bear_id')
+        .get(function(req,res){
+            Bear.findById(req.params.bear_id,function(err,bear){
+                if(err)
+                    res.send(err);
+                res.json(bear);
+            });
+})
+        .put(function(req,res){
+            Bear.findById(req.params.bear_id,function(err,bear){
+                if(err)
+                    res.send(err);
+                
+                bear.name = req.body.name;
+                bear.save(function(err){
+                    if(err)
+                        res.send(err);
+                    res.json({message: 'Beer Updated'});
+                });
+            })
+})
+        .delete(function(req,res){
+            Bear.remove({
+                _id: req.params.bear_id
+            },function(err,bear){
+                if(err)
+                    res.send(err);
+                res.json({message: 'Successfully deleted'});
             });
 });
 
